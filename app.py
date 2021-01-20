6514d1c4e6768f2b1049add95f9478dcf71da562
Paridhi Modi <54037847+paridhimodi@users.noreply.github.com>
2021-01-20 11:30:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ from keras.utils import to_categorical
 
 app = Flask(__name__)
 padding_size = 1000
-model = load_model('model (4).h5')
-model.load_weights('m-weights (6).h5')
+model = load_model('M_model.h5')
+model.load_weights('M_weights (6).h5')
 
 IMAGE_FOLDER = os.path.join('static', 'img_pool')
 app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
